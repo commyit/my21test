6e1041c2eeab64555cb77ba41d052c346046d6f5
commyit <com_myit@126.com>
2020-05-19 14:21:03 +0800

    addemp

---
+++ src/com/myit/entity/Emp.java
@@ -0,0 +1,15 @@
+package com.myit.entity;
+
+public class Emp {
+	private  String  name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
